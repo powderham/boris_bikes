9efc9bbcc8cd8c8f32b1b91d7f1406516c34d85d
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-09-29 14:43:46 +0100

    Create @bike instance variable

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,15 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
+
+  # @bike = bike
+
   def release_bike
     Bike.new
+  end
+
+  def dock_bike(bike)
+    @bike = bike
   end
 end
