01e45e5d439cf68505111bc6ab7d950cb1d3225a
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-09-29 17:17:38 +0100

    Create 'No bike' error message

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,22 +4,28 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'releases working bikes' do
+    subject.dock_bike(Bike.new)
     bike = subject.release_bike
     expect(bike).to be_working
   end
 
   it 'docks a bike' do
+    subject.dock_bike(Bike.new)
     bike = subject.release_bike
-    expect(bike).to be_docked
     expect(subject).to respond_to :dock_bike
     subject.dock_bike(bike)
     expect(subject.bike).to eq bike
   end
 
-  it 'returns instance of the docked bike', focus: true do
+  it 'returns instance of the docked bike' do
+    subject.dock_bike(Bike.new)
     expect(subject).to respond_to :view_bike
     bike = subject.release_bike
     subject.dock_bike(bike)
     expect(subject.view_bike).to be bike
+  end
+
+  it 'should raise an error when no bikes' do
+    expect { subject.release_bike }.to raise_error('No bike')
   end
 end
