87af474e5564aae0bac4eed23c07bd0f46268251
Ed Powderham <edpowderham@outlook.com>
2016-09-29 15:50:47 +0100

    Add view_bike method tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,9 +10,16 @@ describe DockingStation do
 
   it 'docks a bike' do
     bike = subject.release_bike
-    # expect(bike).to be_docked
+    expect(bike).to be_docked
     expect(subject).to respond_to :dock_bike
     subject.dock_bike(bike)
     expect(subject.bike).to eq bike
+  end
+
+  it 'returns instance of the docked bike', focus: true do
+    expect(subject).to respond_to :view_bike
+    bike = subject.release_bike
+    subject.dock_bike(bike)
+    expect(subject.view_bike).to be bike
   end
 end
